889bb4f45a048b7ecf0358826e8758bb1440411f
Cole Hoffbauer <cole.hoffbauer@nielsen.com>
2019-08-12 16:05:41 -0500

    adding a simple calculator tutorial thing

---
+++ calculator_tutorial/test_calculator.py
@@ -0,0 +1,22 @@
+from calculator import Calculator
+
+calc = Calculator()
+
+def test_add_success():
+    res = calc.add(1,4)
+    assert res == 5
+
+def test_subtract_success():
+    res = calc.subtract(3, 9)
+    assert res == -6
+
+def test_multiply_success():
+    res = calc.multiply(3,3)
+    assert res == 9
+
+def test_divide_success():
+    res = calc.divide(15,5)
+    assert res == 3
+
+
+
