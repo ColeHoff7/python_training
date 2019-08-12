fccb43d1821c1d55996deafdbc109331919f6115
Cole Hoffbauer <cole.hoffbauer@nielsen.com>
2019-08-12 16:07:16 -0500

    removing functionality from calculator

--- calculator_tutorial/calculator.py
+++ calculator_tutorial/calculator.py
@@ -1,13 +1,13 @@
 class Calculator:
     
     def add(self, a, b):
-        return a+b
+        pass
 
     def subtract(self, a, b):
-        return a-b
+        pass
     
     def multiply(self, a, b):
-        return a*b
+        pass
     
     def divide(self, a, b):
-        return a/b
+        pass
